83b544a22dfc2e6d7eda6d3dd33d34323faf5ea9
Vignesh <vigneshkannabs@gmail.com>
2026-05-09 15:46:02 +0530

    update api url to render

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://masala-store-production-911e.up.railway.app/api",
+  baseURL: "https://masala-store.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
